40ce52e8fcfda34f2a67d8878f3e805ce552c203
chenxiaoqiang <312933275@qq.com>
2015-10-09 11:35:35 +0800

    init pro

---
+++ XLCommercial/XLCommercial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLCommercial
+//
+//  Created by Mervin on 9/29/15.
+//  Copyright © 2015 Mervin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XLCommercial/XLCommercial/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  XLCommercial
+//
+//  Created by Mervin on 9/29/15.
+//  Copyright © 2015 Mervin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"打印一下");
+    NSLog(@"jenkins ceshi!");
+    NSLog(@"jenkins ceshi ceshi!");
+    NSLog(@"jenkins ceshi ceshi!");    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
