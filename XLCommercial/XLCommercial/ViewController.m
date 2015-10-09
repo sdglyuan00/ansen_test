a912bb7bf3ed52431e2fe8b5142f0d3caec3031e
chenxiaoqiang <312933275@qq.com>
2015-10-09 18:56:13 +0800

    7777777777

--- XLCommercial/XLCommercial/ViewController.m
+++ XLCommercial/XLCommercial/ViewController.m
@@ -16,9 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"sssssss");
-    NSLog(@"sssssss");
-    NSLog(@"sssssss");    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
